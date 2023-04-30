c3b20167027c2d0bf5d62c5d79adf2a7fa94a1a2
SachinPandey582 <sachinpandey582@gmail.com>
2023-04-30 13:13:44 +0530

    added the register part of user

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,13 +1,21 @@
 const mongoose =require("mongoose")
+const bcrypt=require("bcrypt")
 const userSchema=new mongoose.Schema({
-    _id: ObjectId,
-    name: String,
-    email: String,
+    // _id: ObjectId,
+    name: String, 
+    email: String, 
     password: String,
-    dob: Date,
+    dob: String,
     bio: String,
-    posts: [{ type: ObjectId, ref: 'Post' }],
-    friends: [{ type: ObjectId, ref: 'User' }],
-    friendRequests: [{ type: ObjectId, ref: 'User' }]
+    posts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Post' }],
+    friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    friendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+  })
+
+  userSchema.pre("save",async function(next){
+if(this.isModified("password")){
+  this.password=await bcrypt.hash(this.password,10)
+}
+next()
   })
 module.exports=mongoose.model("User",userSchema)
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,7 @@
 const express=require("express")
+const {register} =require("../Controllers/user")
 
 const router=express.Router()
-
+router.route("/register").post(register)
 
 module.exports=router;
